565402e103f357ea02faaaea7755b3f37ce0a9b7
MaricarmenBP <maricarmenbuenfil004@gmail.com>
2025-03-14 00:16:27 -0600

    Nuevos tokens para numeros flotantes

--- compiler/lexer/tokens.py
+++ compiler/lexer/tokens.py
@@ -24,14 +24,15 @@ class Token:
 
 ''' Definir expresiones regulares para los tokens reconocidos '''
 TOKENS = [
-    ("[COMMENT]", r"//[^\n]*"), 
-    ("[KEYWORD]", r"(package|import|func|var|return|int|string)"),
-    ("[IDENTIFIER]", r"[a-zA-Z_][a-zA-Z0-9_]*"), 
-    ("[INTEGER]", r"\d+(\.\d*)?"), 
-    ("[STRING]", r"\"([^\"]|\\\")*\""),   
-    ("[DELIMITER]", r"(\(|\)|{|}|[\[\]\,;.])"),
-    ("[OPERATOR]", r":=|=|[+\-*/%]"),
-    ("[WHITESPACE]", r"\s+")
+    ("[FLOAT]", r"\d+\.\d+"),  # Números flotantes (ej. 3.14)
+    ("[INTEGER]", r"\d+"),  # Números enteros (ej. 42)
+    ("[COMMENT]", r"//[^\n]*"),  # Comentarios (ej. // esto es un comentario)
+    ("[KEYWORD]", r"(package|import|func|var|return|int|string|for|if|else)"),  # Palabras clave
+    ("[IDENTIFIER]", r"[a-zA-Z_][a-zA-Z0-9_]*"),  # Identificadores (ej. i, j, main)
+    ("[STRING]", r"\"([^\"]|\\\")*\""),  # Cadenas de texto (ej. "Hola, mundo")
+    ("[DELIMITER]", r"(\(|\)|{|}|[\[\]\,;.])"),  # Delimitadores (ej. (, ), {, }, [, ], ,, ;, .)
+    ("[OPERATOR]", r"\+\+|--|:=|=|<=|>=|==|!=|[+\-*/%]|&&|\|\|"),  # Operadores (ej. :=, =, +, -, *, /)
+    ("[WHITESPACE]", r"\s+"),  # Espacios en blanco (ignorados)
 ]
 
 
